c665ff85baef8791c1d43cdc0f25a41d1735a38f
Archit Bhonsle <abhonsle2000@gmail.com>
2024-04-15 20:47:23 -0400

    `vectorizer_based_rankers`: overview and keywords

--- trivial_rankers.py
+++ trivial_rankers.py
@@ -8,3 +8,16 @@ class VoteAverageBasedRanker():
 
     def rank(self, user: int, query_movie_ids: np.ndarray) -> np.ndarray:
         return self.movies[self.movies['id'].isin(query_movie_ids)]['vote_average']
+    
+class WeightedVoteBasedRanker():
+    def __init__(self, movies: pd.DataFrame, train_ratings: pd.DataFrame):
+        self.movies = movies
+        v = self.movies['vote_count']
+        r = self.movies['vote_average']
+        m = v.quantile(0.9)
+        c = r.mean()
+        self.movies['weighted_vote'] = (v/(v+m) * r) + (m/(v+m) * c)
+        self.train_ratings = train_ratings
+
+    def rank(self, user: int, query_movie_ids: np.ndarray) -> np.ndarray:
+        return self.movies[self.movies['id'].isin(query_movie_ids)]['weighted_vote']
